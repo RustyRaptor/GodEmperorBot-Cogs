9fb6abe9f8a07ca7e643f0a5b2729a94cc9a8a84
RustyRaptor <ziad.arafat@etvamerica.com>
2020-10-14 16:12:12 -0600

    fixed formatting to be code syntax on discord

--- cmd/cmd.py
+++ cmd/cmd.py
@@ -20,5 +20,6 @@ class CMD(commands.Cog):
         try:
             await ctx.send("```sh\n" + programs[arg](" ".join(arg2) if type(arg2) is tuple else "")) + "\n```"
         except KeyError:
-            await ctx.send(os.popen(arg + " " + " ".join(arg2) if type(arg2) is tuple else "").read())
+            await ctx.send(
+                "```sh\n" + os.popen(arg + " " + " ".join(arg2) if type(arg2) is tuple else "").read() + "\n```")
             return
