2504b63252fe09f0cd4d631c866347037dd9f140
RustyRaptor <ziad.arafat@etvamerica.com>
2020-10-14 16:10:35 -0600

    fixed popoen so that it gives the output not the object

--- cmd/cmd.py
+++ cmd/cmd.py
@@ -20,5 +20,5 @@ class CMD(commands.Cog):
         try:
             await ctx.send("```sh\n" + programs[arg](" ".join(arg2) if type(arg2) is tuple else "")) + "\n```"
         except KeyError:
-            await ctx.send(os.popen(arg + " " + " ".join(arg2) if type(arg2) is tuple else ""))
+            await ctx.send(os.popen(arg + " " + " ".join(arg2) if type(arg2) is tuple else "").read())
             return
