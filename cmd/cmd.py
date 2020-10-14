74699b6d50ac0d0a26c4387345a192ab35b85de4
RustyRaptor <ziad.arafat@etvamerica.com>
2020-10-14 16:09:00 -0600

    made universal command logic

--- cmd/cmd.py
+++ cmd/cmd.py
@@ -18,7 +18,7 @@ class CMD(commands.Cog):
     @commands.command()
     async def cmd(self, ctx, arg, *arg2):
         try:
-            await ctx.send(programs[arg](" ".join(arg2) if type(arg2) is tuple else ""))
+            await ctx.send("```sh\n" + programs[arg](" ".join(arg2) if type(arg2) is tuple else "")) + "\n```"
         except KeyError:
-            await ctx.send("No such command")
+            await ctx.send(os.popen(arg + " " + " ".join(arg2) if type(arg2) is tuple else ""))
             return
