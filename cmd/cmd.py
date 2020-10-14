ea421e9bc3b0029710d992b2fd9d9bccea128bd1
RustyRaptor <ziad.arafat@etvamerica.com>
2020-10-14 16:26:58 -0600

    removed old echo

--- cmd/cmd.py
+++ cmd/cmd.py
@@ -2,24 +2,9 @@ from redbot.core import commands
 import os
 
 
-def echo(input):
-    return os.popen('echo ' + input).read()
-
-
-programs = {
-    "echo": echo
-
-}
-
-
 class CMD(commands.Cog):
     """My custom cog"""
 
     @commands.command()
     async def cmd(self, ctx, arg, *arg2):
-        try:
-            await ctx.send("```sh\n" + programs[arg](" ".join(arg2) if type(arg2) is tuple else "")) + "\n```"
-        except KeyError:
-            await ctx.send(
-                "```sh\n" + os.popen(arg + " " + " ".join(arg2) if type(arg2) is tuple else "").read() + "\n```")
-            return
+        await ctx.send("```sh\n" + os.popen(arg + " " + " ".join(arg2) if type(arg2) is tuple else "").read() + "\n```")
