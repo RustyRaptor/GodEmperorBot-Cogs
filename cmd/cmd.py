ac84f9322357f57e9ac8a4caa3cbe179d5307a12
RustyRaptor <ziad.arafat@etvamerica.com>
2020-10-14 15:44:50 -0600

    converting echo to using system echo

--- cmd/cmd.py
+++ cmd/cmd.py
@@ -1,12 +1,14 @@
 from redbot.core import commands
+import os
 
 
 def echo(input):
-    return input
+    return os.popen('echo' + input).read()
 
 
 programs = {
     "echo": echo
+
 }
 
 
@@ -14,7 +16,7 @@ class CMD(commands.Cog):
     """My custom cog"""
 
     @commands.command()
-    async def cmd(self, ctx, arg, arg2):
+    async def cmd(self, ctx, arg, *arg2):
         try:
             await ctx.send(programs[arg](arg2))
         except KeyError:
