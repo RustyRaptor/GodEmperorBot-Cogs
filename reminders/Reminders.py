8d5ff94ec05f1d1e67931b9b34de14e8b4207202
RustyRaptor <ziada@nmsu.edu>
2021-04-15 21:37:34 -0600

    appears to be a timezone issue

--- reminders/Reminders.py
+++ reminders/Reminders.py
@@ -2,6 +2,7 @@ from redbot.core import commands
 # import json
 import pause
 from datetime import datetime
+from dateutil import tz
 
 
 class Reminders(commands.Cog):
@@ -9,12 +10,14 @@ class Reminders(commands.Cog):
 
     @commands.command()
     async def set_reminder(self, ctx, remind_time, label):
+        nm_tz = tz.gettz("America/Denver")
         remind_time = str(remind_time).split("/")
         try:
             dt = datetime(year=int(remind_time[0]),
                           month=int(remind_time[1]),
                           day=int(remind_time[2]),
-                          hour=int(remind_time[3]), minute=int(remind_time[4]))
+                          hour=int(remind_time[3]), minute=int(remind_time[4]),
+                          tzinfo=nm_tz)
         except IndexError:
             ctx.send(
                 "{author.mention} please enter remind time as "
